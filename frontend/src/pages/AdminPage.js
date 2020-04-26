415437c486ab5a15cf2a9f65794c2b2c1005a2ee
SVLaursen <simonvlaursen@gmail.com>
2020-04-26 23:33:01 +0200

    Added semantic-ui kit to the frontend, and started connecting backend

--- frontend/src/pages/AdminPage.js
+++ frontend/src/pages/AdminPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import ProductList from '../components/AdminContent';
 
 const AdminPage = () => {
     return(
         <div>
             <h3>Admin Page</h3>
+            <ProductList></ProductList>
         </div>
     );
 }
